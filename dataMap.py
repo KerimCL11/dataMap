0263ccb261e2879907ee243af9f9570a6f9c5760
Kerim Cisneros de La Llave <111947587+KerimCL11@users.noreply.github.com>
2025-10-15 17:38:49 -0700

    Display map data in a dataframe
    Added functionality to display the map data in a dataframe.

--- dataMap.py
+++ dataMap.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 import streamlit as st
 import numpy as np 
-
+import matplotlib as mt;
 map_data = pd.DataFrame( 
 np.random.randn(100, 2) / [50,50] + [37.76, -122.4], 
 columns = ['lat', 'lon']) 
 st.map(map_data) 
 
 # print(map_data)
+
 st.dataframe(map_data);
